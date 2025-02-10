7b9c5df4b3b3b930553f5befa4ced8b39a1f5951
Abhimanyu <abhimanyukarki0@gmail.com>
2025-02-10 00:28:05 -0600

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Board from './Components/Board/Board';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
